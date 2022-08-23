f56fc8cf217409ced97e32d3528801491f69a562
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-24 03:55:57 +0600

    Update ceaser decrypt.py

--- ceaser decrypt.py
+++ ceaser decrypt.py
@@ -4,3 +4,14 @@ key=int(input('Enter the key: '))
 nword=''
 
 ascii=nwa=0
+
+for i in word:
+    if i.isupper():
+        ascii=ord(i)
+        nwa=(ascii-key-65)%26+65
+    if i.islower():
+        ascii=ord(i)
+        nwa=(ascii-key-97)%26+97
+    nword+=chr(nwa)
+    
+print('Decrypted code: ',nword)
