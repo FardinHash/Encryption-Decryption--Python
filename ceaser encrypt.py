afaf816044cd3d1effd26d6a0c98bf67f58ee59e
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-24 03:39:07 +0600

    Update ceaser encrypt.py

--- ceaser encrypt.py
+++ ceaser encrypt.py
@@ -1,5 +1,5 @@
-word=input("Enter the text:")
-key=int(input("Enter the key:"))
+word=input('Enter the text: ')
+key=int(input('Enter the key:"))
 no_word=''
 
 ascii=no_word=0
@@ -8,3 +8,11 @@ for i in word:
     if i.isupper():
         ascii=ord(i)
         nowa=(ascii+key-65)%26+65
+        
+    if i.islower():
+        ascii=ord(i)
+        nowa=(ascii+key-97)%26+97
+        
+    no_word+=chr(nowa)
+    
+ print('Encrypted code: ',no_word)
