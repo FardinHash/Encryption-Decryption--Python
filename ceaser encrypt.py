76b7db6bb2a7a27a205f9f2c0b7ad8d216483f13
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-24 03:36:12 +0600

    Update ceaser encrypt.py

--- ceaser encrypt.py
+++ ceaser encrypt.py
@@ -1,2 +1,10 @@
 word=input("Enter the text:")
 key=int(input("Enter the key:"))
+no_word=''
+
+ascii=no_word=0
+
+for i in word:
+    if i.isupper():
+        ascii=ord(i)
+        nowa=(ascii+key-65)%26+65
